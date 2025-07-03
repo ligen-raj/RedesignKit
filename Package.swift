482a63a9dd58408758dca180aa92702eb399b4e9
Ligen Raj <rnlrligen@gmail.com>
2025-07-03 23:57:03 +0530

    Add SwiftPM support with static library

--- Package.swift
+++ Package.swift
@@ -20,6 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "RedesignKit",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
     ]
 )
